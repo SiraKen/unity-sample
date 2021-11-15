917cb12200814e65636f5a076a5a47e5d94b018a
Kento Shirasawa <shirasawa.kento@gmail.com>
2021-11-15 21:14:38 +0900

    first commit

---
+++ Assets/Scripts/SphereScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour
+{
+    public bool isGameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameOver = false;
+
+        // 乱数でスタート位置を決める
+        int value = Random.Range(0, 10 + 1);
+        Debug.Log(value);
+        if (value % 2 == 0)
+        {
+            // Right
+            this.transform.Translate(new Vector3(6f, 0, 0));
+        }
+        else
+        {
+            // Left
+            this.transform.Translate(new Vector3(-6f, 0, 0));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isGameOver)
+        {
+            if (this.transform.position.y < -3)
+            {
+                Debug.Log("ゲームオーバー");
+                isGameOver = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TestCubeBehaviourScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCubeBehaviourScript : MonoBehaviour
+{
+    public float maxPosY = 1.5f;
+    public float minPosY = -1.5f;
+    public float moveSpeed = 1.0f;
+    public bool isDropping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello from Visual Studio.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* 上下に動くだけ */
+        /*
+        float objPosY = this.transform.position.y;
+        if (!isDropping)
+        {
+            if (objPosY < maxPosY)
+            {
+                this.transform.Translate(new Vector3(0, 0.01f, 0));
+            } else
+            {
+                isDropping = true;
+            }
+            
+        } else
+        {
+            if (minPosY < objPosY)
+            {
+                this.transform.Translate(new Vector3(0, -0.01f, 0));
+            }
+            else
+            {
+                isDropping = false;
+            }
+        }
+        */
+        //this.transform.Translate(new Vector3(0.01f, 0, 0));
+
+        // Speed Changer
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            moveSpeed = 2.0f;
+        }
+        else
+        {
+            moveSpeed = 1.0f;
+        }
+
+        // Move
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(new Vector3(moveSpeed * -0.05f, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(new Vector3(moveSpeed * 0.05f, 0, 0));
+        }
+    }
+}
