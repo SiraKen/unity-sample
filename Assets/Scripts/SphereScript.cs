7e6cdcb83a867fb5c298846e2f1a0bfb71d04a12
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-04-13 12:37:19 +0900

    feat: game over and retry

--- Assets/Scripts/SphereScript.cs
+++ Assets/Scripts/SphereScript.cs
@@ -5,25 +5,19 @@ using UnityEngine;
 public class SphereScript : MonoBehaviour
 {
     public bool isGameOver;
+    public GameObject canvas;
+    public Rigidbody rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         isGameOver = false;
+        canvas = GameObject.Find("Canvas");
+        canvas.GetComponent<Canvas>().enabled = false;
 
-        // 乱数でスタート位置を決める
-        int value = Random.Range(0, 10 + 1);
-        Debug.Log(value);
-        if (value % 2 == 0)
-        {
-            // Right
-            this.transform.Translate(new Vector3(6f, 0, 0));
-        }
-        else
-        {
-            // Left
-            this.transform.Translate(new Vector3(-6f, 0, 0));
-        }
+        rigidbody = GetComponent<Rigidbody>();
+
+        SetStartPosition();
     }
 
     // Update is called once per frame
@@ -40,9 +34,39 @@ public class SphereScript : MonoBehaviour
             if (collision.gameObject.name == "Floor")
             {
                 Debug.Log("Game Over");
+
+                rigidbody.velocity = Vector3.zero;
+                rigidbody.angularVelocity = Vector3.zero;
+
+                canvas.GetComponent<Canvas>().enabled = true;
                 isGameOver = true;
             }
         }
 
+    }
+
+    public void SetStartPosition()
+    {
+        Debug.Log("SetStartPosition");
+        
+        // 乱数でスタート位置を決める
+        int value = Random.Range(0, 10 + 1);
+
+        if (value % 2 == 0)
+        {
+            // Right
+            // this.transform.Translate(new Vector3(6f, 0, 0));
+            this.transform.position = new Vector3(6f, 10f, 0);
+        }
+        else
+        {
+            // Left
+            // this.transform.Translate(new Vector3(-6f, 0, 0));
+            this.transform.position = new Vector3(-6f, 10f, 0);
+        }
+
+        GameObject canvas = GameObject.Find("Canvas");
+        canvas.GetComponent<Canvas>().enabled = false;
+        isGameOver = false;
     }
 }
